dc766bccf8701780088114f51349efcfc4b59498
sagar-joshi83 <joshhisagar9375@gmail.com>
2023-04-10 15:22:22 +0545

    Piano updated

--- self piano/piano.js
+++ self piano/piano.js
@@ -1,30 +1,72 @@
-// const synth = new Tone.Synth().toDestination();
-
-// // Play a note
-// synth.triggerAttackRelease("C4", "8n");
+"use strict";
 
 let keys = document.querySelector(".keys");
 
 let octave_no;
-for (octave_no = 1; octave_no <= 4; octave_no++) {
+for (octave_no = 1; octave_no <= 1; octave_no++) {
   keys.innerHTML += `
-  <div class="octave-1">
-  <div class="white-keys">
-    <div class="white key"></div>
-    <div class="white key"></div>
-    <div class="white key"></div>
-    <div class="white key"></div>
-    <div class="white key"></div>
-    <div class="white key"></div>
-    <div class="white key"></div>
-  </div>
-  <div class="black-keys">
-    <div class="black key"></div>
-    <div class="black key"></div>
-    <div class="black key"></div>
-    <div class="black key"></div>
-    <div class="black key"></div>
-  </div>
-  </div>
-`;
+  <div class="octaves">
+          <div class="white-keys">
+            <div class="key1 white"></div>
+            <div class="key3 white"></div>
+            <div class="key5 white"></div>
+            <div class="key6 white"></div>
+            <div class="key8 white"></div>
+            <div class="key10 white"></div>
+            <div class="key12 white"></div>
+          </div>
+
+          <div class="black-keys">
+            <div class="key2 black"></div>
+            <div class="key4 black"></div>
+            <div class="key7 black"></div>
+            <div class="key9 black"></div>
+            <div class="key11 black"></div>
+          </div>
+        </div>
+  `;
+}
+
+// console.log(Tone.js);
+
+// import * as Tone from "tone";
+
+// let synth = Tone.Synth().toDestination();
+
+// // Play a note
+
+// console.log(Tone.context.state);
+
+// let c4synth = function () {
+//   // if (Tone.context.state != "running") {
+//   //   Tone.start();
+//   // }
+
+//   // AudioContext().resume();
+// };
+
+// c4synth();
+let i = 0;
+let b3 = 261.6256;
+
+const freqRatio = Math.pow(2, 1 / 12);
+
+// console.log(freqRatio);
+
+for (; i < 12; i++) {
+  const btn = document.querySelector(`.key${i + 1}`);
+
+  let freq = b3 * Math.pow(freqRatio, i);
+
+  btn.addEventListener("click", function () {
+    const synth = new Tone.Synth().toDestination();
+
+    // let newNote = refC4 * keyNo;
+
+    console.log(btn);
+    synth.triggerAttackRelease(freq, "8n");
+    console.log(freq);
+  });
+  // console.log(btn.innerHTML);
+  // console.log(freq);
 }
--- self piano/tempCodeRunnerFile.js
+++ self piano/tempCodeRunnerFile.js
@@ -1,3 +1,25 @@
-let heading = document.querySelector("#company");
+let keys = document.querySelector(".keys");
 
-// keys.innerHTML += "hello";
+// let octave_no;
+// for (octave_no = 1; octave_no <= 4; octave_no++) {
+//   keys.innerHTML += `
+//   <div class="octave-1">
+//   <div class="white-keys">
+//     <div class="white key"></div>
+//     <div class="white key"></div>
+//     <div class="white key"></div>
+//     <div class="white key"></div>
+//     <div class="white key"></div>
+//     <div class="white key"></div>
+//     <div class="white key"></div>
+//   </div>
+//   <div class="black-keys">
+//     <div class="black key"></div>
+//     <div class="black key"></div>
+//     <div class="black key"></div>
+//     <div class="black key"></div>
+//     <div class="black key"></div>ss
+//   </div>
+//   </div>
+// `;
+// }
